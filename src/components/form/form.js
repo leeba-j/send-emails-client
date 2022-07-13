1b3ecd49ac2831e265201ddbd06f6e27d21c2f0e
Julius <Julius.leeba@absa.africa>
2022-07-13 13:07:20 +0200

    Clear fields

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -8,21 +8,29 @@ const Form = () => {
     const { handleSubmit, register } = useForm();
     const [createUser, { data, loading, error }] = useMutation(CREATE_USER_AND_SEND_EMAIL);
 
+    const clearInputFields = () => {
+        document.querySelectorAll('input[type="text"]').forEach(inputField => {
+            inputField.value = '';
+        })
+    };
+
     const onSubmit = ({ name, email, height, weight }) => {
         createUser({
             variables: { userDetails: { name, email, height: parseFloat(height), weight: parseFloat(weight) } },
             refetchQueries: [{ query: ALL_USERS }]
         });
+
+        clearInputFields();
     };
 
     return (<form onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder='Your name' {...register("name", { required: true })} />
+        <input type={'text'} placeholder='Your name' {...register("name", { required: true })} />
 
-        <input placeholder='Your email' {...register("email", { required: true })} />
+        <input type={'text'} placeholder='Your email' {...register("email", { required: true })} />
 
-        <input placeholder='Weight (/kg)'  {...register("weight", { required: true })} />
+        <input type={'text'} placeholder='Weight (/kg)'  {...register("weight", { required: true })} />
 
-        <input placeholder='Height (/m)' {...register("height", { required: true })} />
+        <input type={'text'} placeholder='Height (/m)' {...register("height", { required: true })} />
         <input type="submit" value={'Submit'} />
     </form>);
 }
